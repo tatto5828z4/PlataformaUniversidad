735a618ce246d5ca8001fdc14eb2b109547a993e
Jose-Pablo-Hernandez <alexlubu15@gmail.com>
2020-03-20 11:47:19 -0600

    Horario

--- Plataforma_Universidad.sql
+++ Plataforma_Universidad.sql
@@ -10,6 +10,7 @@ Encargado_Carrera varchar(50) not null,
 Estado_Carrera varchar(1) not null
 
 )Engine =InnoDB, default char set=latin1;
+
 create table Cursos(
 
 	ID_Curso int primary key,
@@ -20,3 +21,46 @@ create table Cursos(
     Estado_Curso char(1)
 )Engine= InnoDB, default char set= latin1;
 
+create table Sede(
+	ID_Sede int primary key,
+    Nombre_Sede varchar(50) not null,
+    Direccion_Sede varchar(100) not null,
+    Numero_Clases int,
+	Numero_Alumnos int,
+    Numero_Maestros int,
+    Numero_Carreras int,
+    Comentario varchar(100),
+    Estado_Sede char(1)
+)Engine= InnoDB, default char set= latin1;
+
+create table Maestro(
+	ID_Maestro int primary key,
+    CUI_Maestro int not null,
+    Nombre_Maestro varchar(60) not null,
+    Apellido_Maestro varchar(60) not null,
+    Fecha_Nacimiento date not null,
+    Telefono_Maestro int not null,
+    Direccion_Maestro varchar(80) not null,
+    Correo_Maestro varchar(50) not null,
+    Genero_Mestro char(1) not null,
+    Estado_Civil char(1),
+    Sueldo_Maestro float,
+    Estado_Maestro char(1) not null,
+    
+     -- Autenticacion 
+    Username_Maestro varchar(80) not null,
+    Password_Maestro varchar(80) not null,
+    
+    ID_Curso int not null,
+    foreign key (ID_Curso) references Cursos(ID_Curso)
+)Engine= InnoDB, default char set= latin1;
+create table Horario(
+	ID_Horario int primary key,
+    ID_Carrera int not null,
+    ID_Curso int not null,
+    ID_Maestro int not null,
+    
+    foreign key (ID_Carrera) references Carreras(ID_Carrera),
+    foreign key (ID_Curso) references Cursos(ID_Curso),
+    foreign key (ID_Maestro) references Maestro(ID_Maestro)
+)Engine= InnoDB, default char set= latin1;
